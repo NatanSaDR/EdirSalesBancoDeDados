c96b801f662e5a6ed98554e99c011abf1271d8fe
natansantana555 <natan.santana555@hotmail.com>
2025-03-14 15:33:12 -0300

    Criando tela de login

--- front-end/EdirSalesBancoDeDados/src/pages/LoginPage.jsx
+++ front-end/EdirSalesBancoDeDados/src/pages/LoginPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import './LoginPage.css'
 const LoginPage = () => {
   return (
-    <div>teste</div>
+    <div className='main-login-page'>
+      <h1>Login</h1>
+      <form className='login-form'>
+        <input type="text" placeholder="Usuário" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
   )
 }
 
